d2b7f8b7f315cf8d23e01122783e3000c96324a3
beattris <beatriceebirim@gmail.com>
2024-01-11 16:11:29 +0100

    used select() to read data from the counterReducer store

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,7 +1,11 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer } from '@ngrx/store';
 
 const initialState = 0;
 
-export const counterReducer = createReducer(
-    initialState
-);
+export const counterReducer = createReducer(initialState);
+
+// BELOW IS AN ALTERNATIVE WAY OF CREATING A REDUCER 
+
+// export function counterReducer(state = initialState) {
+//   return state;
+// }
