09d1e59b5f626eb81d844518704ef17af3f87a3e
beattris <beatriceebirim@gmail.com>
2024-01-11 16:55:13 +0100

    created an Increment action and dispatched it

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,10 +1,14 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { increment } from './counter.action';
 
 const initialState = 0;
 
-export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1)
+);
 
-// BELOW IS AN ALTERNATIVE WAY OF CREATING A REDUCER 
+// BELOW IS AN ALTERNATIVE WAY OF CREATING A REDUCER
 
 // export function counterReducer(state = initialState) {
 //   return state;
