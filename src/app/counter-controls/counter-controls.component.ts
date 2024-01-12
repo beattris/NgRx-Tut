47a7faf0e01bbb807001351a068444328fb4a933
beattris <beatriceebirim@gmail.com>
2024-01-12 09:41:37 +0100

    worked on the alternative way of creating actions

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { increment } from '../store/counter.action';
+import { IncrementAction } from '../store/counter.action';
 
 @Component({
   selector: 'app-counter-controls',
@@ -10,8 +10,14 @@ import { increment } from '../store/counter.action';
 export class CounterControlsComponent {
   constructor(private store: Store) {}
 
+  // increment() {
+  //   this.store.dispatch(increment({value: 2}));
+  // }
+
+  // BELOW IS AN ALTERNATIVE WAY OF IMPLEMENTING THE 'increment' METHOD
+
   increment() {
-    this.store.dispatch(increment({value: 2}));
+    this.store.dispatch(new IncrementAction(2));
   }
 
   decrement() {}
--- src/app/store/counter.action.ts
+++ src/app/store/counter.action.ts
@@ -1,6 +1,19 @@
-import { createAction, props } from "@ngrx/store";
+import { Action, createAction, props } from "@ngrx/store";
 
-export const increment = createAction(
-    '[Counter] Increment',
-    props<{value: number}>()
-);
+// export const increment = createAction(
+//     '[Counter] Increment',
+//     props<{value: number}>()
+// );
+
+// BELOW IS AN ALTERNATIVE WAY OF IMPLEMENTING AN ACTION
+export const INCREMENT = '[Counter] Increment';
+
+export class IncrementAction implements Action {
+    readonly type = INCREMENT;
+
+    constructor (public value: number){}
+}
+
+//  CREATING A GROUPED TYPE FOR OUR ACTIONS
+
+export type CounterActions = IncrementAction;
