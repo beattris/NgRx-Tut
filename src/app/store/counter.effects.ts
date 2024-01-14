c1d282f307795adbaef8fcbdeeefbe94c9ffb803
beattris <beatriceebirim@gmail.com>
2024-01-14 14:16:40 +0100

    created a counterEffect

---
+++ src/app/store/counter.effects.ts
@@ -0,0 +1,22 @@
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { decrement, increment } from './counter.action';
+import { tap } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class CounterEffects {
+  saveCount = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(increment, decrement),
+        tap((action) => {
+          console.log(action);
+          localStorage.setItem('count', action.value.toString());
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(private actions$: Actions) {}
+}
